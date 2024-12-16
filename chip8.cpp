4176428e3095bec042428550a3838b5b27b928e3
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2024-12-16 12:05:51 +0530

    Implemented chip 8 specifications

--- chip8.cpp
+++ chip8.cpp
@@ -2,11 +2,13 @@
 
 class Chip8 {
     public:
-        uint8_t registers[16]{};
         uint8_t memory[4096]{};
-        uint16_t index{};
+        uint32_t display[64 * 32]{}; // Monochrome display
         uint16_t pc{};
+        uint16_t index{};
         uint16_t stack[16]{};
-        uint8_t sp{};
-
+        uint8_t sp{}; // Stack pointer
+        uint8_t delayTimer{}; // Decrements at 60Hz
+        uint8_t soundTimer{}; // Decrements at 60Hz, buzzes when non-zero
+        uint8_t registers[16]{}; // v0-vF
 };
