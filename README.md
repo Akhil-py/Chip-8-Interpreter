49a972a211a387f0a2f3bdb9e5f8337b969c4d19
Akhil-py <ramshankar.akhil@gmail.com>
2024-12-21 19:46:38 +0530

    Changed dev environment from windows to linux

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Chip-8-Interpreter-py
 
 Writing a chip-8 emulator to expand my horizons in computer architecture.
+
+This project is a WIP.
+
+Graphics are generated using SDL2.
+
+
+Sources:
+https://tobiasvl.github.io/blog/write-a-chip-8-emulator/ (Primary Source)
+https://austinmorlan.com/posts/chip8_emulator/ 
--- window.cpp
+++ window.cpp
@@ -6,14 +6,34 @@
 
 const int WIDTH = 64;
 const int HEIGHT = 32;
-const int SCALE = 10;
+const int SCALE = 20;
 
+class Window {
+public:
+	Window(int width, int height, int scale) {
+		SDL_Window* window = SDL_CreateWindow("chip8",
+									SDL_WINDOWPOS_CENTERED,
+									SDL_WINDOWPOS_CENTERED,
+									WIDTH*SCALE, HEIGHT*SCALE, 0);
 
+		//SDL_Renderer* renderer = SDL_CreateRenderer()
+	}
 
+	~Window() {
+
+	}
+
+	void update(void const* buffer) {
+
+	}
+
+};
+
+/**
+ * The main function is only used to test the SDL library
+ */
 int main(int argc, char *argv[])
 {
-	
-
 	// returns zero on success else non-zero
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		printf("error initializing SDL: %s\n", SDL_GetError());
